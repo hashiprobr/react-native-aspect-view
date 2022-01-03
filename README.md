cabb097995821654a4bf21a11dd5fe9ac6c986ee
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-01-03 19:00:54 -0300

    Fix calculations and complete documentation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 react-native-aspect-view
 ========================
+
+**A React Native View that keeps a consistent aspect ratio in all platforms**
+
+React Native offers the [`aspectRatio` layout
+prop](https://reactnative.dev/docs/layout-props#aspectratio) to control the size
+of the undefined dimension of a component. However, [React Native for
+Web](https://necolas.github.io/react-native-web/) implements this prop using the
+[`aspect-ratio` CSS
+property](https://developer.mozilla.org/en-US/docs/Web/CSS/aspect-ratio), which
+is similar but not equivalent.
+
+The `AspectView` React Component is a `View` that uses `aspectRatio` in Android
+and iOS but does not use `aspect-ratio` in the web. Instead, it uses `onLayout`
+to emulate the behavior.
+
+
+Install
+-------
+
+```
+npm install @hashiprobr/react-native-aspect-view
+```
--- src/WebAspectView.js
+++ src/WebAspectView.js
@@ -2,29 +2,110 @@ import React, { useState } from 'react';
 
 import { View } from 'react-native';
 
+function isNonNegative(value) {
+    return Number.isFinite(value) && value >= 0;
+}
+
+function hasWidth(style) {
+    return isNonNegative(style.width);
+}
+
+function hasHeight(style) {
+    return isNonNegative(style.height);
+}
+
+function hasBasis(style) {
+    return isNonNegative(style.flexBasis);
+}
+
+function hasGrow(style) {
+    return Number.isFinite(style.flexGrow) && style.flexGrow > 0;
+}
+
+function hasStretch(style, parentStyle) {
+    switch (style.alignSelf) {
+        case 'flex-start':
+        case 'flex-end':
+        case 'center':
+        case 'baseline':
+            return false;
+        case 'stretch':
+            return true;
+        default:
+            return parentStyle['align-content'] === 'stretch';
+    }
+}
+
 export default function WebAspectView(props) {
     const [column, setColumn] = useState(true);
-    const [basis, setBasis] = useState(0);
+    const [value, setValue] = useState(0);
+
+    function calculateWidth(event) {
+        return event.layout.height * props.ratio;
+    }
+
+    function calculateHeight(event) {
+        return event.layout.width / props.ratio;
+    }
+
+    function setMaximumAspect(event) {
+        const width = calculateWidth(event);
+        const height = calculateHeight(event);
+        const rowArea = event.layout.width * height;
+        const columnArea = width * event.layout.height;
+        if (rowArea !== columnArea) {
+            if (rowArea < columnArea) {
+                setColumn(true);
+                setValue(width);
+            } else {
+                setColumn(false);
+                setValue(height);
+            }
+        }
+    }
+
+    function setColumnAspect(event) {
+        const width = calculateWidth(event);
+        setColumn(true);
+        setValue(width);
+    }
+
+    function setRowAspect(event) {
+        const height = calculateHeight(event);
+        setColumn(false);
+        setValue(height);
+    }
 
     function onLayout({ nativeEvent }) {
         const style = { ...props.style };
-        if ('width' in style) {
-            if (!('height' in style)) {
-                setColumn(false);
-                setBasis(nativeEvent.layout.width);
+        const parentStyle = getComputedStyle(nativeEvent.target.parentElement);
+        const direction = parentStyle['flex-direction'];
+        if (direction === 'row' || direction === 'row-reverse') {
+            if (hasWidth(style) || hasBasis(style)) {
+                setRowAspect(nativeEvent);
+            } else {
+                if (hasHeight(style) || hasStretch(style, parentStyle)) {
+                    if (hasGrow(style)) {
+                        setMaximumAspect(nativeEvent);
+                    } else {
+                        setColumnAspect(nativeEvent);
+                    }
+                } else {
+                    setRowAspect(nativeEvent);
+                }
             }
         } else {
-            if ('height' in style) {
-                setColumn(true);
-                setBasis(nativeEvent.layout.height);
+            if (hasHeight(style) || hasBasis(style)) {
+                setColumnAspect(nativeEvent);
             } else {
-                const parentStyle = getComputedStyle(nativeEvent.target.parentElement);
-                if (parentStyle['flex-direction'] === 'column') {
-                    setColumn(true);
-                    setBasis(nativeEvent.layout.height);
+                if (hasWidth(style) || hasStretch(style, parentStyle)) {
+                    if (hasGrow(style)) {
+                        setMaximumAspect(nativeEvent);
+                    } else {
+                        setRowAspect(nativeEvent);
+                    }
                 } else {
-                    setColumn(false);
-                    setBasis(nativeEvent.layout.width);
+                    setColumnAspect(nativeEvent);
                 }
             }
         }
@@ -34,10 +115,13 @@ export default function WebAspectView(props) {
     }
 
     const style = { ...props.style };
-    if (column) {
-        style.width = basis * props.ratio;
-    } else {
-        style.height = basis / props.ratio;
+
+    if (value) {
+        if (column) {
+            style.width = value;
+        } else {
+            style.height = value;
+        }
     }
 
     return (
